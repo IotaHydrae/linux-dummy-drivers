92abbe0a0528e3688701a7e701ae53e83ebe3ec1
iotah <writeforever@foxmail.com>
2025-01-12 06:57:58 +0800

    use devm alloc drm device
    Signed-off-by: iotah <writeforever@foxmail.com>

--- drm/drm.c
+++ drm/drm.c
@@ -18,6 +18,7 @@
 #include <drm/drm_fbdev_generic.h>
 #include <drm/drm_gem_atomic_helper.h>
 #include <drm/drm_gem_dma_helper.h>
+#include <drm/drm_simple_kms_helper.h>
 #include <drm/drm_managed.h>
 
 #define DRV_NAME "dummy-drm"
@@ -27,9 +28,9 @@ struct dummy_device {
     struct device *dev;
 };
 
-struct dummy_drm_priv {
-    struct drm_device *drm;
-    struct dummy_dev *dev;
+struct dummy_drm_dev {
+    const struct drm_display_mode mode;
+    struct drm_device drm;
 };
 
 struct dirty_area {
@@ -49,11 +50,32 @@ struct dummy_display {
 
 static struct dummy_device *ddev;
 
-static const struct dummy_display display = {
-    .xres = 480,
-    .yres = 320,
-    .bpp = 16,
-    .fps = 24,
+static inline struct dummy_drm_dev *drm_to_dummy_drm_dev(struct drm_device *drm)
+{
+    return container_of(drm, struct dummy_drm_dev, drm);
+}
+
+enum drm_mode_status dummy_drm_pipe_mode_valid(struct drm_simple_display_pipe *pipe,
+					      const struct drm_display_mode *mode)
+{
+    // struct
+    return 0;
+}
+
+static const struct drm_simple_display_pipe_funcs dummy_display_pipe_funcs = {
+    .mode_valid = NULL,
+    .enable = NULL,
+    .disable = NULL,
+    .update = NULL,
+    .begin_fb_access = NULL,
+    .end_fb_access = NULL,
+    .reset_plane = NULL,
+    .duplicate_plane_state = NULL,
+    .destroy_plane_state = NULL,
+};
+
+static const struct drm_display_mode dummy_mode = {
+    DRM_SIMPLE_MODE(480, 320, 85, 55),
 };
 
 DEFINE_DRM_GEM_DMA_FOPS(dummy_drm_fops);
@@ -69,32 +91,35 @@ static const struct drm_driver dummy_drm_driver = {
     .minor = 0,
 };
 
-static int dummy_drm_alloc(struct dummy_device *ddev)
+static int dummy_drm_dev_init_with_formats(struct dummy_drm_dev *ddrm,
+                const struct drm_simple_display_pipe_funcs *funcs,
+                const uint32_t *formats, unsigned int formats_count,
+                const struct drm_display_mode *mode, size_t buf_size)
 {
-    struct dummy_drm_priv *priv;
-    int width, height, bpp, rotate;
+    pr_info("%s\n", __func__);
+    return 0;
+}
+
+static int dummy_drm_dev_init(struct dummy_drm_dev *ddrm,
+                const struct drm_simple_display_pipe_funcs *funcs,
+                const struct drm_display_mode *mode)
+{
+    ssize_t bufsize = mode->vdisplay * mode->hdisplay * sizeof(u16);
+
+    ddrm->drm.mode_config.preferred_depth = 16;
 
     pr_info("%s\n", __func__);
 
-    width  = display.xres;
-    height = display.yres;
-    bpp    = display.bpp;
-    rotate = display.rotate;
-
-    priv = devm_drm_dev_alloc(ddev->dev, &dummy_drm_driver,
-        struct dummy_drm_priv, drm);
-    if (IS_ERR(priv))
-        return PTR_ERR(priv);
-
-    drm_dev_register(priv->drm, 0);
-    // drm_fbdev_generic_setup(priv->drm, 0);
-
-    return 0;
+    return dummy_drm_dev_init_with_formats(ddrm, funcs, NULL, 0, mode, bufsize);
 }
 
 static int __init dummy_drm_init(void)
 {
-    pr_info("%s\n", __func__);
+    struct dummy_drm_dev *ddrm;
+    struct drm_device *drm;
+    int rc;
+
+    pr_info("\n\n\n%s\n", __func__);
 
     ddev = kzalloc(sizeof(*ddev), GFP_KERNEL);
     if (!ddev) {
@@ -114,9 +139,27 @@ static int __init dummy_drm_init(void)
         goto err_free_class;
     }
 
-    dummy_drm_alloc(ddev);
+    ddrm = devm_drm_dev_alloc(ddev->dev, &dummy_drm_driver,
+                    struct dummy_drm_dev, drm);
+    if (IS_ERR(ddrm)) {
+        pr_err("failed to allocate drm\n");
+        goto err_free_dev;
+    }
+    drm = &ddrm->drm;
+    dev_set_drvdata(ddev->dev, drm);
+
+    rc = dummy_drm_dev_init(ddrm, &dummy_display_pipe_funcs, &dummy_mode);
+    if (rc) {
+        pr_err("failed to init drm dev\n");
+        goto err_free_drm;
+    }
+
     return 0;
 
+err_free_drm:
+    drm_dev_put(&ddrm->drm);
+err_free_dev:
+    device_destroy(ddev->class, MKDEV(0, 0));
 err_free_class:
     class_destroy(ddev->class);
 err_free_ddev:
@@ -126,6 +169,7 @@ err_free_ddev:
 
 static void __exit dummy_drm_exit(void)
 {
+    struct drm_device *drm = dev_get_drvdata(ddev->dev);
     pr_info("%s\n", __func__);
 
     if (ddev->dev)
@@ -133,6 +177,9 @@ static void __exit dummy_drm_exit(void)
 
     if (ddev->class)
         class_destroy(ddev->class);
+
+    if (drm)
+        drm_dev_put(drm);
 
     kfree(ddev);
 }
